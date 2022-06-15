6ace8c0285efe6477cf25bad40669d3b47a57ea2
mamutjan turkel <mturkel@totalwine.com>
2022-06-14 23:48:23 -0400

    DIG-71364 Add a function to toggle hidden class on modal

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log('Engineering Training!');
 const modalButton = document.getElementById('modalButton');
 console.log("modalButton", modalButton);
+modalButton.addEventListener("click", whenClicked);
+function whenClicked() {
+    console.log("Clicked!");
+    let modalContainer = document.getElementById("modal");
+    modalContainer.classList.toggle("hidden");
+    console.log(modalContainer.classList);
+}
