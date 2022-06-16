a887a8ba6ddc956745b6098202d65326d756d090
mamutjan turkel <mturkel@totalwine.com>
2022-06-15 22:34:18 -0400

    DIG-71364 Add closeModalButton

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ console.log("modalButton", modalButton);
 modalButton.addEventListener("click", whenClicked);
 function whenClicked() {
     console.log("Clicked!");
-    let modalContainer = document.getElementById("modal");
+    const modalContainer = document.getElementById("modal");
     modalContainer.classList.toggle("hidden");
     console.log(modalContainer.classList);
 }
+const closeModalButton = document.getElementsByClassName("closeModal");
+
+closeModalButton[0].addEventListener("click", whenCloseClicked);
+
+console.log("closeModal", closeModalButton);
+
+function whenCloseClicked() {
+  console.log("Clicked Close!");
+  const modalContainer = document.getElementById("modal");
+  modalContainer.classList.toggle("hidden");}
