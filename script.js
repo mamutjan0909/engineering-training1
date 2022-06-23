119895b8ac1da2258cdc95b68c0dcd4a9f497a86
mamutjan turkel <mturkel@totalwine.com>
2022-06-22 20:55:05 -0400

    DIG-71560 String Literals to Elements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-console.log('Engineering Training!');
-const modalButton = document.getElementById('modalButton');
+console.log("Engineering Training!");
+
+const modalButton = document.getElementById("modalButton");
 console.log("modalButton", modalButton);
 modalButton.addEventListener("click", whenClicked);
 function whenClicked() {
-    console.log("Clicked!");
-    const modalContainer = document.getElementById("modal");
-    modalContainer.classList.toggle("hidden");
-    console.log(modalContainer.classList);
+  console.log("Clicked!");
+  const modalContainer = document.getElementById("modal");
+  modalContainer.classList.toggle("hidden");
+  console.log(modalContainer.classList);
 }
 const closeModalButton = document.getElementsByClassName("closeModal");
 
@@ -15,47 +16,48 @@ closeModalButton[0].addEventListener("click", whenCloseClicked);
 console.log("closeModal", closeModalButton);
 
 function whenCloseClicked() {
-    console.log("Clicked Close!");
-    const modalContainer = document.getElementById("modal");
-    modalContainer.classList.toggle("hidden");
+  console.log("Clicked Close!");
+  const modalContainer = document.getElementById("modal");
+  modalContainer.classList.toggle("hidden");
 }
-let titles = [
-    "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
-    "Create index.html with basic html markup and perform first commit",
-    "Add anchor tags for each completed subtasks",
-    "Make unordered list of anchor tags",
-    "Add the TWM logo as an image to the beginning of the body of the page",
-    "Add style attributes to img tag to setting the height and width of the logo",
-    "Add a head, add a style tag to the head, add a class to style tag, move logo styles to style tag, remove inline styles from logo, add class to the logo",
-    "Add a selector inside style tag that targets the list items and removes the bullet",
-    "Pseudo-selectors - Add hover styling to list elements",
-    "UI Libraries - Add Bootstrap to your page, add check icons to your list, and convert your list into a bootstrap list-group",
+const titles = [
+  "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
+  "Create index.html with basic html markup and perform first commit",
+  "Add anchor tags for each completed subtasks",
+  "Make unordered list of anchor tags",
+  "Add the TWM logo as an image to the beginning of the body of the page",
+  "Add style attributes to img tag to setting the height and width of the logo",
+  "Add a head, add a style tag to the head, add a class to style tag, move logo styles to style tag, remove inline styles from logo, add class to the logo",
+  "Add a selector inside style tag that targets the list items and removes the bullet",
+  "Pseudo-selectors - Add hover styling to list elements",
+  "UI Libraries - Add Bootstrap to your page, add check icons to your list, and convert your list into a bootstrap list-group",
 ];
-let links = [
-    "https://totalwine.atlassian.net/browse/DIG-70749",
-    "https://totalwine.atlassian.net/browse/DIG-70771",
-    "https://totalwine.atlassian.net/browse/DIG-70804",
-    "https://totalwine.atlassian.net/browse/DIG-70805",
-    "https://totalwine.atlassian.net/browse/DIG-70905",
-    "https://totalwine.atlassian.net/browse/DIG-70918",
-    "https://totalwine.atlassian.net/browse/DIG-70993",
-    "https://totalwine.atlassian.net/browse/DIG-71030",
-    "https://totalwine.atlassian.net/browse/DIG-71062",
-    "https://totalwine.atlassian.net/browse/DIG-71085",
+const links = [
+  "https://totalwine.atlassian.net/browse/DIG-70749",
+  "https://totalwine.atlassian.net/browse/DIG-70771",
+  "https://totalwine.atlassian.net/browse/DIG-70804",
+  "https://totalwine.atlassian.net/browse/DIG-70805",
+  "https://totalwine.atlassian.net/browse/DIG-70905",
+  "https://totalwine.atlassian.net/browse/DIG-70918",
+  "https://totalwine.atlassian.net/browse/DIG-70993",
+  "https://totalwine.atlassian.net/browse/DIG-71030",
+  "https://totalwine.atlassian.net/browse/DIG-71062",
+  "https://totalwine.atlassian.net/browse/DIG-71085",
 ];
-
 
 const jirasArray = [];
 for (let index = 0; index < titles.length; index++) {
-    jirasArray.push({
-        titles: titles[index],
-        links: links[index],
-    });
+  jirasArray.push({
+    title: titles[index],
+    link: links[index],
+  });
 }
-var list = document.getElementsByClassName("grid-container");
+const list = document.getElementsByClassName("grid-container");
 
-jirasArray.forEach(element => {
-  console.log(element); 
-  var listElement = document.createElement("li");
-list[0].prepend(listElement);
+jirasArray.forEach((element) => {
+  const listElement = document.createElement("li");
+  listElement.innerHTML = `<li class="grid-item"><i class="bi bi-check-circle-fill"></i><a
+    href="${element.link}">${element.title}</a></li>`;
+  list[0].append(listElement);
+  console.log(element);
 });
