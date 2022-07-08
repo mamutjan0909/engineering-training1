601db15e27d50eddf879899499358fca298804b4
mamutjan turkel <mturkel@totalwine.com>
2022-07-08 08:11:39 -0400

    DIG-71922 JavaScript Classes

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 console.log("Engineering Training!");
 
-// let dataLoaded = false;
-
-// function loadData() {
-
-//   whenClicked();
-
-//   setTimeout(function () {
-
-//     renderData().then((response) => {
-//       dataLoaded = true;
-//       list[0].innerHTML = response;
-//       whenCloseClicked();
-//       return response;
-//     });
-//     console.log("Data loaded");
-//   }, 2000);
-
-//   if (dataLoaded == true) {
-//     console.log("Date Already Loaded");
-//     return;
-//   }
-// }
 const titles = [
   "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
   "Create index.html with basic html markup and perform first commit",
@@ -46,28 +24,9 @@ const links = [
   "https://totalwine.atlassian.net/browse/DIG-71062",
   "https://totalwine.atlassian.net/browse/DIG-71085",
 ];
-const jirasArray = [];
-for (let index = 0; index < titles.length; index++) {
-  jirasArray.push({
-    title: titles[index],
-    link: links[index],
-  });
-}
+
 const list = document.getElementsByClassName("grid-container");
 
-// function renderData() {
-//   return new Promise((resolve) => {
-//     let response = "";
-//     jirasArray.forEach((element) => {
-//       const { link, title } = element;
-//       response += `<li class="item"><a href= ${link}>
-//       <i class="bi bi-check-circle-fill">
-//       </i> ${title}
-//       </a></li>`;
-//     });
-//     resolve(response);
-//   });
-// }
 let dataLoaded = false;
 
 const utils = {
@@ -90,7 +49,7 @@ const utils = {
       console.log("Date Already Loaded");
       return;
     }
-    //whenClicked();
+    whenClicked();
 
     this.renderData().then(function (response) {
       setTimeout(function () {
@@ -102,13 +61,9 @@ const utils = {
   },
 };
 const modalButton = document.getElementById("modalButton");
-modalButton.addEventListener("click",function () {
-  
-
-utils.loadData()
+modalButton.addEventListener("click", function () {
+  utils.loadData();
 });
-
-
 
 function whenClicked() {
   console.log("Clicked!");
@@ -124,3 +79,19 @@ function whenCloseClicked() {
 }
 const closeModalButton = document.getElementsByClassName("closeModal");
 closeModalButton[0].addEventListener("click", whenCloseClicked);
+
+class JiraHandler {
+  constructor(links, titles) {
+    this.links = links;
+    this.titles = titles;
+  }
+}
+
+const jiraHandler = new JiraHandler(links, titles);
+const jirasArray = [];
+for (let index = 0; index < titles.length; index++) {
+  jirasArray.push({
+    title: titles[index],
+    link: links[index],
+  });
+}
