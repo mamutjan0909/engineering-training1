e4a2eae01023903420ee82e551eec9493c773b59
mamutjan turkel <mturkel@totalwine.com>
2022-08-02 09:29:05 -0400

    DIG-72156 Async / Await

--- script.js
+++ script.js
@@ -1,118 +1,131 @@
-console.log("Engineering Training!");
+(async function () {
+  console.log("Engineering Training!");
 
-const titles = [
-  "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
-  "Create index.html with basic html markup and perform first commit",
-  "Add anchor tags for each completed subtasks",
-  "Make unordered list of anchor tags",
-  "Add the TWM logo as an image to the beginning of the body of the page",
-  "Add style attributes to img tag to setting the height and width of the logo",
-  "Add a head, add a style tag to the head, add a class to style tag, move logo styles to style tag, remove inline styles from logo, add class to the logo",
-  "Add a selector inside style tag that targets the list items and removes the bullet",
-  "Pseudo-selectors - Add hover styling to list elements",
-  "UI Libraries - Add Bootstrap to your page, add check icons to your list, and convert your list into a bootstrap list-group",
-];
-const links = [
-  "https://totalwine.atlassian.net/browse/DIG-70749",
-  "https://totalwine.atlassian.net/browse/DIG-70771",
-  "https://totalwine.atlassian.net/browse/DIG-70804",
-  "https://totalwine.atlassian.net/browse/DIG-70805",
-  "https://totalwine.atlassian.net/browse/DIG-70905",
-  "https://totalwine.atlassian.net/browse/DIG-70918",
-  "https://totalwine.atlassian.net/browse/DIG-70993",
-  "https://totalwine.atlassian.net/browse/DIG-71030",
-  "https://totalwine.atlassian.net/browse/DIG-71062",
-  "https://totalwine.atlassian.net/browse/DIG-71085",
-];
+  const titles = [
+    "Create and publish a public repository in GitHub under your personal account named 'Engineering Training'",
+    "Create index.html with basic html markup and perform first commit",
+    "Add anchor tags for each completed subtasks",
+    "Make unordered list of anchor tags",
+    "Add the TWM logo as an image to the beginning of the body of the page",
+    "Add style attributes to img tag to setting the height and width of the logo",
+    "Add a head, add a style tag to the head, add a class to style tag, move logo styles to style tag, remove inline styles from logo, add class to the logo",
+    "Add a selector inside style tag that targets the list items and removes the bullet",
+    "Pseudo-selectors - Add hover styling to list elements",
+    "UI Libraries - Add Bootstrap to your page, add check icons to your list, and convert your list into a bootstrap list-group",
+  ];
 
-let jiraTemplate = {
-  icon: "bi bi-check-circle-fill",
-};
-let errorJiraTemplate = {
-  icon: "bi bi-x-circle",
-};
-function getRandomNum(max) {
-  return Math.floor(Math.random() * max);
-}
-function getIcon() {
-  let rNum = getRandomNum(3);
-  console.log(rNum, rNum >= 1);
-  return rNum >= 1 ? jiraTemplate : errorJiraTemplate;
-}
-class JiraHandler {
-  constructor(links, titles) {
-    this.links = links;
-    this.titles = titles;
-    this.jirasObject = [];
-    this.creatJiraObject();
+  const links = [
+    "https://totalwine.atlassian.net/browse/DIG-70749",
+    "https://totalwine.atlassian.net/browse/DIG-70771",
+    "https://totalwine.atlassian.net/browse/DIG-70804",
+    "https://totalwine.atlassian.net/browse/DIG-70805",
+    "https://totalwine.atlassian.net/browse/DIG-70905",
+    "https://totalwine.atlassian.net/browse/DIG-70918",
+    "https://totalwine.atlassian.net/browse/DIG-70993",
+    "https://totalwine.atlassian.net/browse/DIG-71030",
+    "https://totalwine.atlassian.net/browse/DIG-71062",
+    "https://totalwine.atlassian.net/browse/DIG-71085",
+  ];
+
+  let jiraTemplate = {
+    icon: "bi bi-check-circle-fill",
+  };
+
+  let errorJiraTemplate = {
+    icon: "bi bi-x-circle",
+  };
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
-  creatJiraObject() {
-    for (let index = 0; index < this.titles.length; index++) {
-      let icon = getIcon();
-      this.jirasObject.push({
-        title: this.titles[index],
-        link: this.links[index],
-        ...icon,
-      });
+
+  function getIcon() {
+    let rNum = getRandomInt(3);
+    return rNum >= 1 ? jiraTemplate : errorJiraTemplate;
+  }
+
+  class JiraHandler {
+    constructor(links, titles) {
+      this.links = links;
+      this.titles = titles;
+      this.jirasObject = [];
+      this.createJiraObject();
+    }
+    createJiraObject() {
+      for (let index = 0; index < this.titles.length; index++) {
+        let icon = getIcon();
+        this.jirasObject.push({
+          title: this.titles[index],
+          link: this.links[index],
+          ...icon,
+        });
+      }
     }
   }
-}
 
-const jiraHandler = new JiraHandler(links, titles);
-function initModalButton() {
-  var dataLoaded = false;
-  const openModalButton = document.getElementById("modalButton");
-  openModalButton.addEventListener("click", () => {
-    if (dataLoaded === true) {
-      return;
-    }
-    utils.loadData(() => {
-      dataLoaded = true;
+  const jiraHandler = new JiraHandler(links, titles);
+
+  function initModalButton() {
+    return new Promise((resolve) => {
+    var dataLoaded = false;
+    const modalButton = document.getElementById("modalButton");
+    modalButton.addEventListener("click", () => {
+      if (dataLoaded === false) {
+        resolve();
+      }
     });
-  });
-}
-initModalButton();
-const utils = {
-  loadData: function (callback) {
-    setTimeout(toggleFunction, 1000);
+    })
+      .then(utils.loadData(() => {
+        dataLoaded = true;
+      }));
+  }
 
-    setTimeout(() => {
-      this.renderData().then((response) => {
-        list[0].innerHTML = response;
+  const utils = {
+    loadData: function (callback) {
+      setTimeout(whenCloseClicked, 1000)
 
-        return response;
+      setTimeout(() => {
+        utils.renderData().then((response) => {
+          list[0].innerHTML = response;
+          return response;
+        })
+      }, 2000)
+      callback();
+
+      setTimeout(() => {
+        let modalContainer = document.getElementById("modal");
+        modalContainer.classList.add("hidden");
+    }, 2000)
+    },
+    renderData: function () {
+      return new Promise((resolve) => {
+        let response = "";
+        jiraHandler.jirasObject.forEach((element) => {
+          let { link, title, icon } = element;
+          response += `<li class="item"><a href= ${link}> 
+        <i class="${icon}">
+        </i> ${title} 
+        </a></li>`;
+        });
+        resolve(response);
       });
-    }, 2000);
-    callback();
+    },
+  };
 
-    setTimeout(() => {
-      let modalContainer = document.getElementById("modal");
-      modalContainer.classList.add("hidden");
-    }, 4000);
-  },
-  renderData: function () {
-    let response = "";
-    return new Promise((resolve, reject) => {
-      jiraHandler.jirasObject.forEach((element) => {
-        let { link, title, icon } = element;
-        console.log(icon);
-        response += `<li class="item"><a href= ${link}> 
-          <i class="${icon}">
-          </i> ${title} 
-          </a></li>`;
-      });
-      resolve(response);
-    });
-  },
-};
-function toggleFunction() {
-  let modalContainer = document.getElementById("modal");
-  modalContainer.classList.toggle("hidden");
-}
-const openModalButton = document.getElementById("modalButton");
-const closeModalButton = document.getElementsByClassName("closeModal");
-const list = document.getElementsByClassName("grid-container");
+  function whenCloseClicked() {
+    console.log("Clicked Close!");
+    const modalContainer = document.getElementById("modal");
+    modalContainer.classList.toggle("hidden");
+  }
 
-closeModalButton.item(0).addEventListener("click", toggleFunction);
-console.log("modalButton", openModalButton);
-console.log("closeModalButton", closeModalButton);
+  const closeModalButton = document.getElementsByClassName("closeModal");
+
+  closeModalButton[0].addEventListener("click", whenCloseClicked);
+
+  const list = document.getElementsByClassName("grid-container");
+
+  console.log("BEFORE initModalButton is called");
+  await initModalButton();
+  console.log("AFTER initModalButton is called");
+
+})();
