b409792bc14b0e60d28edce31740563f0cf585e9
mamutjan turkel <mturkel@totalwine.com>
2022-08-24 14:47:11 -0400

    DIG-72390 Express Routing Intro

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use("/", router);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Engineer Training app listening on port ${port}`);
+});
